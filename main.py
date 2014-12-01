83280e688dd09ea1571a2e4dd814cb674cbf8525
Wes Gilleland <wes.gilleland@gmail.com>
2014-12-01 18:54:52 -0500

    Refactored into som emethods

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from buttercoin.client import ButtercoinClient
 
 from config import api_key, api_secret
 
+
 is_in_a_terminal = sys.stdin.isatty()
 
 
@@ -35,6 +36,47 @@ balance = client.get_balances()
 count = 0
 frequency = 1.0
 old = {'last': 0, 'bid': 0, 'ask': 0}
+
+
+def decide_colors():
+	global last, changed, bid, ask
+	if last < old['last']:
+		old['last'] = last
+		last = colors.FAIL + last + colors.ENDC
+		changed = True
+	elif last > old['last']:
+		old['last'] = last
+		last = colors.OKGREEN + str(last) + colors.ENDC
+		changed = True
+	if bid < old['bid']:
+		old['bid'] = bid
+		bid = colors.FAIL + str(bid) + colors.ENDC
+		changed = True
+	elif bid > old['bid']:
+		old['bid'] = bid
+		bid = colors.OKGREEN + str(bid) + colors.ENDC
+		changed = True
+	if ask < old['ask']:
+		old['ask'] = ask
+		ask = colors.FAIL + str(ask) + colors.ENDC
+		changed = True
+	elif ask > old['ask']:
+		old['ask'] = ask
+		ask = colors.OKGREEN + str(ask) + colors.ENDC
+		changed = True
+
+
+def update_display():
+	clear_screen()
+	print("Current balances: ${0}, {1}BTC".format(balance['USD'], balance['BTC']))
+	print("Last Buy: ${1}\nLast Bid: ${2}\nCurrent Asking Price: ${3}\nTime: {4}".format(currency, last, bid, ask,
+																						 time.strftime("%H:%M:%S")))
+	print("{0}BTC -> USD (current market price): ${1}".format(round(balance['BTC'], 8),
+															  round(balance['BTC'] * old['bid'], 4)))
+	print("${0} -> BTC (current market price): {0}BTC".format(round(balance['USD'], 2),
+															  round(balance['USD'] / old['ask'], 8)))
+
+
 while True:
 	try:
 		count += 1
@@ -46,45 +88,10 @@ while True:
 		bid = ticker['bid']
 		ask = ticker['ask']
 
-		if last < old['last']:
-			old['last'] = last
-			last = colors.FAIL + last + colors.ENDC
-			changed = True
-		elif last > old['last']:
-			old['last'] = last
-			last = colors.OKGREEN + str(last) + colors.ENDC
-			changed = True
-
-		if bid < old['bid']:
-			old['bid'] = bid
-			bid = colors.FAIL + str(bid) + colors.ENDC
-			changed = True
-		elif bid > old['bid']:
-			old['bid'] = bid
-			bid = colors.OKGREEN + str(bid) + colors.ENDC
-			changed = True
-
-		if ask < old['ask']:
-			old['ask'] = ask
-			ask = colors.FAIL + str(ask) + colors.ENDC
-			changed = True
-		elif ask > old['ask']:
-			old['ask'] = ask
-			ask = colors.OKGREEN + str(ask) + colors.ENDC
-			changed = True
+		decide_colors()
 
 		if changed or True:
-			clear_screen()
-			print("Current balances: ${0}, {1}BTC".format(balance['USD'], balance['BTC']))
-			print(
-				"Last Buy: ${1}\nLast Bid: ${2}\nCurrent Asking Price: ${3}\nTime: {4}".format(currency, last, bid, ask,
-																							   time.strftime(
-																								   "%H:%M:%S")))
-			print("{0}BTC -> USD (current market price): ${1}".format(round(balance['BTC'], 8),
-																	  round(balance['BTC'] * old['bid'], 4)))
-			print("${0} -> BTC (current market price): {0}BTC".format(round(balance['USD'], 2),
-																	  round(balance['USD'] / old['ask'], 8)))
-		# print("Update {0}".format(count))
+			update_display()
 
 		time.sleep(frequency)
 
